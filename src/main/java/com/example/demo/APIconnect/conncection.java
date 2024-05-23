456a1c0f15e77c6418c83baf35c4985f05e7db24
smmoon <wja04097@naver.com>
2024-05-23 22:15:51 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/demo/APIconnect/conncection.java
@@ -0,0 +1,21 @@
+package com.example.demo.APIconnect;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Clock;
+
+
+@RestController
+public class conncection {
+    @CrossOrigin(origins  = "/")
+    @GetMapping("/api/hello")
+    //@RequestMapping("/api")
+    public String Hello(){
+        return "hi hi";
+    }
+}
