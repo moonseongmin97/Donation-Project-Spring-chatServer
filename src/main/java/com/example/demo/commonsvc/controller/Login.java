8f190c52e0a3d723c3a6351c12db3d9425c72423
smmoon <wja04097@naver.com>
2024-10-28 20:30:50 +0900

    재재시작

---
+++ src/main/java/com/example/demo/commonsvc/controller/Login.java
@@ -0,0 +1,56 @@
+package com.example.demo.commonsvc.controller;
+
+
+import com.example.demo.DemoApplication;
+import com.example.demo.commonsvc.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+
+
+@RestController
+public class Login {
+
+
+    @Autowired
+    private LoginService loginService;
+
+    @CrossOrigin(origins  = "/")
+    @GetMapping("/api/hello")
+    //@RequestMapping("/api")
+    public String Hello(){
+
+    	//String a=DemoApplication.findMember2();
+    	
+        return "hi";
+    }
+    @CrossOrigin(origins  = "/")
+    @PostMapping("/api/login")
+    //@RequestMapping("/api")
+    public String Login(HttpServletRequest request, @RequestBody Map<String, Object> paramMap){
+
+        System.out.println("찍힘="+paramMap);
+
+
+        return "";
+    }
+
+    @CrossOrigin(origins  = "/")
+    @PostMapping("/api/Join")
+    //@RequestMapping("/api")
+    public String Join(HttpServletRequest request, @RequestBody Map<String, Object> paramMap){
+        System.out.println("결과 값1111===="+paramMap);
+
+
+        String b =  loginService.Login(paramMap);
+
+        System.out.println("결과 값222===="+b);
+
+
+        return "";
+    }
+
+}
