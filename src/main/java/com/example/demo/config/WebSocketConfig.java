644f7770ce113fba29e6a507938abd027b0381e9
smmoon <wja04097@naver.com>
2026-04-07 00:06:01 +0900

    Fix: WebSocket allowedOrigins에 myhopebridge.duckdns.org 추가

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -26,7 +26,7 @@ public class WebSocketConfig implements WebSocketConfigurer, WebMvcConfigurer {
         System.out.println("WebSocket handler registered");
         registry.addHandler(customWebSocketHandler(), "/chat")
         		.addInterceptors(new JwtHandshakeInterceptor()) // JWT 인터셉터
-        		.setAllowedOrigins("http://localhost:3000"); // React 앱 도메인;
+        		.setAllowedOrigins("http://localhost:3000", "https://myhopebridge.duckdns.org", "http://myhopebridge.duckdns.org"); // React 앱 도메인;
                 //.setAllowedOrigins("*"); // 개발용, 운영 시 특정 도메인으로 변경
     }
 
